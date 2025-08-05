7c1a46244f29ad158ba50eef501a2d661bedbe9e
Armin <amoraghah@gmail.com>
2025-08-06 00:11:03 +0330

    feat: fix nested spaghetti code

--- charReplace.py
+++ charReplace.py
@@ -1,20 +1,25 @@
-def charReplace(name1,name2):
+# Basic replacements for characters
+def replace_char(name):
+    LowerCaseName = name.lower()
     replacements = {'i': '1', 'a': '4', 's': '5'}
-    # Basic replacements for characters
-    def replace_char(name):
-        result = ""
-        for char in name:
-            if char in replacements:
-                result += replacements[char]
-            else:
-                result += char
+    result = ""
+    for char in LowerCaseName:
+        if char in replacements:
+            result += replacements[char]
+        else:
+            result += char
+    return result
 
-        return result
+# print(replace_char("Armin Sara"))  # Example usage
+    # 4rm1n 54r4
+names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hannah", "Ivan", "Julia"]
+count = 0
+for i in names:
+    count += 1
+    print("Mame_number:", count)
+    print("Original_name:", i)
+    print("Modified_name:", replace_char(i))
 
-    name1 = replace_char(name1)
-    name2 = replace_char(name2)
-    print(name1, name2)
-    return name1, name2
 
-charReplace("armin", "sara")
-# 4rm1n 54r4
+print("total:", count)
+
